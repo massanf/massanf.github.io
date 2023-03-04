646c7eaf00ab9f7925de325e4def626841596dd2
trombiano1 <trombiano1@gmail.com>
2023-03-04 16:48:49 +0900

    fix: menubar position

--- components/template.tsx
+++ components/template.tsx
@@ -35,39 +35,39 @@ export default function Template(props: { children: string | number | boolean | 
                 height: '100%',
                 backgroundSize: 'cover'
             }}>
+                <div className={styles.navbarButtonWrap}>
+                    <a onClick={() => setState({ isPaneOpen: true })} className={styles.navbarButton}>
+                        <FontAwesomeIcon icon={faBars} />
+                    </a>
+                    <SlidingPane
+                        // className="some-custom-class"
+                        className={styles.overlay}
+                        isOpen={state.isPaneOpen}
+                        // title="Hey, it is optional pane title.  I can be React component too."
+                        // subtitle="Optional subtitle."
+                        from="left"
+                        hideHeader
+                        width="300px"
+                        onRequestClose={() => {
+                            // triggered on "<" on left top click or on outside click
+                            setState({ isPaneOpen: false });
+                        }}
+                    >
+                        <div className={styles.contentArea}>
+                            <div className={sidebarStyle.sidebar}>
+                                <Link href="/">- Home</Link>
+                                <Link href="/education">- Education</Link>
+                                <Link href="/">- Experience</Link>
+                                <Link href="/works">- Works</Link>
+                                <Link href="/">- Qualifications</Link>
+                                <Link href="/">- Fun</Link>
+                            </div>
+                        </div>
+                    </SlidingPane>
+                </div>
                 <div className={styles.maincontainer}>
                     <div>
                         <div className={styles.name}>
-                            <div className={styles.navbarButtonWrap}>
-                                <a onClick={() => setState({ isPaneOpen: true })} className={styles.navbarButton}>
-                                    <FontAwesomeIcon icon={faBars} />
-                                </a>
-                                <SlidingPane
-                                    // className="some-custom-class"
-                                    className={styles.overlay}
-                                    isOpen={state.isPaneOpen}
-                                    // title="Hey, it is optional pane title.  I can be React component too."
-                                    // subtitle="Optional subtitle."
-                                    from="left"
-                                    hideHeader
-                                    width="300px"
-                                    onRequestClose={() => {
-                                        // triggered on "<" on left top click or on outside click
-                                        setState({ isPaneOpen: false });
-                                    }}
-                                >
-                                    <div className={styles.contentArea}>
-                                        <div className={sidebarStyle.sidebar}>
-                                            <Link href="/">- Home</Link>
-                                            <Link href="/education">- Education</Link>
-                                            <Link href="/">- Experience</Link>
-                                            <Link href="/works">- Works</Link>
-                                            <Link href="/">- Qualifications</Link>
-                                            <Link href="/">- Fun</Link>
-                                        </div>
-                                    </div>
-                                </SlidingPane>
-                            </div>
                             <div>
                                 <span>Welcome to the page of:</span>
                                 <h1 className={inter.className}>Masayuki Fujita</h1>
