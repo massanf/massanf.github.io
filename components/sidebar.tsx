ac4c1a98bbd6c23808cd196dafcf550b6814b50d
trombiano1 <trombiano1@gmail.com>
2023-03-05 00:48:00 +0900

    change: works -> opensource works

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -14,7 +14,7 @@ export default function Sidebar() {
         <div className={styles.contentArea}>
             <div className={sidebarStyle.sidebar}>
                 <Link href="/">- Home</Link>
-                <Link href="/works">- Works</Link>
+                <Link href="/works">- Open-source Works</Link>
                 <Link href="/education">- Education</Link>
                 <Link href="/carrer">- Carrer</Link>
                 <Link href="/qualifications">- Qualifications</Link>
--- components/template.tsx
+++ components/template.tsx
@@ -58,7 +58,7 @@ export default function Template(props: { children: string | number | boolean | 
                         <div className={styles.contentArea}>
                             <div className={sidebarStyle.sidebar}>
                                 <Link href="/">- Home</Link>
-                                <Link href="/works">- Works</Link>
+                                <Link href="/works">- Open-source Works</Link>
                                 <Link href="/education">- Education</Link>
                                 <Link href="/carrer">- Carrer</Link>
                                 <Link href="/qualifications">- Qualifications</Link>
