45fcd752a1f2efbc7de58220537d5d592af41d2d
trombiano1 <trombiano1@gmail.com>
2023-03-12 18:16:51 +0900

    fixing image path

--- pages/qualifications.tsx
+++ pages/qualifications.tsx
@@ -3,10 +3,10 @@ import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import qualificationsStyles from '@/styles/Qualifications.module.css'
 import Template from '../components/template'
-import atcoder from '/logos/atcoder.svg'
-import cf from '/logos/cf.png'
-import eiken from '/logos/eiken.png'
-import toeic from '/logos/toeic.png'
+import atcoder from '../imgs/logos/atcoder.svg'
+import cf from '../imgs/logos/cf.png'
+import eiken from '../imgs/logos/eiken.png'
+import toeic from '../imgs/logos/toeic.png'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faUpRightFromSquare } from "@fortawesome/free-solid-svg-icons"
 import { motion } from 'framer-motion';
